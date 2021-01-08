90ddfb1ef0bd84bd1727e24bb5c8d8f3db4a8331
enzo_niodo <Enzo34490@hotmail.fr>
2021-01-08 16:29:23 +0100

    Ecran entrainement FIN

---
+++ app/src/main/java/com/ptut/fitrainer/End_TrainingActivity.java
@@ -0,0 +1,14 @@
+package com.ptut.fitrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class End_TrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end__training);
+    }
+}
