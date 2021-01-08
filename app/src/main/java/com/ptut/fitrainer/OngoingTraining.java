ccbd6670542404425bdb6428bf7820c8a0f633a1
Antoine De La Fuente <antoinejdelafuente@gmail.com>
2021-01-08 14:58:27 +0100

    activities vides pour entrainement en cours

---
+++ app/src/main/java/com/ptut/fitrainer/OngoingTraining.java
@@ -0,0 +1,14 @@
+package com.ptut.fitrainer;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OngoingTraining extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ongoing_training);
+    }
+}
