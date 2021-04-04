45a2a47308c46dc0fe8acc81543bf0addc58c986
ReyMysterioo <HeritierL3004>
2021-04-05 01:03:03 +0200

    tous les textEdit mis comme il faut + spinner + intent deconnexion

--- app/src/main/java/com/ptut/fitrainer/MainMenuActivity.java
+++ app/src/main/java/com/ptut/fitrainer/MainMenuActivity.java
@@ -2,7 +2,14 @@ package com.ptut.fitrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.UUID;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -10,5 +17,14 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        Button bouton = (Button) findViewById(R.id.activity_main_menu_logout_button);
+        bouton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainMenuActivity.this, ConnectActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
